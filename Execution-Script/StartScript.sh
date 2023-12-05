2f2a8fd9e8d54737885644254c96a48f824e67bf
Dipali Dhanwade <dipali.dhanwade@coditas.com>
2023-12-05 16:48:03 +0530

    changes made in startscript.sh

--- Execution-Script/StartScript.sh
+++ Execution-Script/StartScript.sh
@@ -43,5 +43,29 @@ sudo apt-get install docker-compose -y >/dev/null && echo "** Successfully Insta
 
 
 cd "$path"
+
+
+# Stop and remove containers, including volumes, if they are running
+if [ ! -z "shopping-project_frontend_1" ] || [ ! -z "shopping-project_backend_1" ]; then
+    sudo docker-compose down --volumes
+fi
+
+# Remove frontend and backend images, if they exist
+if [ ! -z "shopping-project_frontend" ]; then
+    sudo docker rmi -f shopping-project_frontend
+fi
+
+if [ ! -z "shopping-project_backend" ]; then
+    sudo docker rmi -f shopping-project_backend
+fi
+
+# Check if containers and images are removed, then rebuild them
+if [ -z "$(sudo docker ps -q -f name=frontend)" ] && [ -z "$(sudo docker ps -q -f name=backend)" ] && \
+   [ -z "$(sudo docker images -q frontend_image)" ] && [ -z "$(sudo docker images -q backend_image)" ]; then
+    sudo docker-compose up -d
+else
+    echo "Error: Containers or images still exist. Manual intervention may be required."
+fi
+
 sudo docker-compose up -d || echo "error in compose file"
 
