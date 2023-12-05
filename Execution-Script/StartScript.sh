25a1b904d57c1c9bdc47ed2ad2f1fcf7bcc32ee1
dipali-coditas <130266261+dipali-coditas@users.noreply.github.com>
2023-12-05 17:14:40 +0530

    Update StartScript.sh

--- Execution-Script/StartScript.sh
+++ Execution-Script/StartScript.sh
@@ -25,6 +25,7 @@ getVariables()
 
 setVariables
 
+sudo apt-get update
 #Install Nginx Server
 sudo apt-get install nginx -y >/dev/null || { echo "Failed to Install Nginx Server"; exit 1; }
 
@@ -37,7 +38,6 @@ sudo systemctl restart nginx
 echo "*** Successfully Configured Nginx ***"
 
 cd
-
 sudo apt-get install docker -y >/dev/null && echo "** Successfully Installed Docker **" || { echo "Failed to Install Docker"; exit 1; }
 sudo apt-get install docker-compose -y >/dev/null && echo "** Successfully Installed Docker-Compose **" || { echo "Failed to Install Docker-Copmose"; exit 1; }
 
